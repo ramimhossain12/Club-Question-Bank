9bb7f49ac3c1be3ef2c274e28db8e4b99f53a37b
ramimhossain12 <ramimhossain6242@gmail.com>
2020-10-21 21:53:47 +0600

    UI Compleet

--- app/src/main/java/com/example/programmingquestionbank/StudentLoginActivity.java
+++ app/src/main/java/com/example/programmingquestionbank/StudentLoginActivity.java
@@ -2,13 +2,35 @@ package com.example.programmingquestionbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class StudentLoginActivity extends AppCompatActivity {
+
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_login);
+
+
+        textView = findViewById(R.id.textId1);
+
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(StudentLoginActivity.this,SudentSignUpActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
+
+
 }
---
+++ app/src/main/java/com/example/programmingquestionbank/SudentSignUpActivity.java
@@ -0,0 +1,14 @@
+package com.example.programmingquestionbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SudentSignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sudent_sign_up);
+    }
+}
