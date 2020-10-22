f4cdc6b6df7a035c7e31d02d66062bddfb57d5a9
ramimhossain12 <ramimhossain6242@gmail.com>
2020-10-22 23:36:46 +0600

    Sign Up Updated

---
+++ app/src/main/java/com/example/programmingquestionbank/StudentDashBoardActivity.java
@@ -0,0 +1,14 @@
+package com.example.programmingquestionbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentDashBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_dash_board);
+    }
+}
--- app/src/main/java/com/example/programmingquestionbank/StudentLoginActivity.java
+++ app/src/main/java/com/example/programmingquestionbank/StudentLoginActivity.java
@@ -1,36 +1,117 @@
 package com.example.programmingquestionbank;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class StudentLoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
+public class StudentLoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView textView;
+    private static final int RC_SIGN_IN = 101;
+    private EditText signInEmailEditText, signInPasswordEditText;
+    private TextView signUpTextView;
+    private Button signInButton;
+    private Button facebook;
+    private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_login);
+        mAuth = FirebaseAuth.getInstance();
 
 
-        textView = findViewById(R.id.textId1);
+        signInEmailEditText = findViewById(R.id.signInEmailEditTextID);
+        signInPasswordEditText = findViewById(R.id.signInpasswordEditTextID);
+
+        signUpTextView = findViewById(R.id.signUpTextViewID);
+        signInButton = findViewById(R.id.signInButtonID);
+        progressBar = findViewById(R.id.progressbarID);
 
 
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(StudentLoginActivity.this,SudentSignUpActivity.class);
-                startActivity(i);
-            }
-        });
+        signUpTextView.setOnClickListener(this);
+        signInButton.setOnClickListener(this);
+
+
     }
 
 
+    @Override
+    public void onClick(View v) {
 
+        switch (v.getId()) {
+
+            case R.id.signInButtonID:
+
+                userLogin();
+                break;
+
+            case R.id.signUpTextViewID:
+
+                Intent intent = new Intent(getApplicationContext(), SudentSignUpActivity.class);
+                startActivity(intent);
+                break;
+        }
+
+    }
+
+    private void userLogin() {
+
+        String email = signInEmailEditText.getText().toString().trim();
+        String password = signInPasswordEditText.getText().toString().trim();
+
+        //checking the validity of the email
+        if (email.isEmpty()) {
+            signInEmailEditText.setError("Enter an email address");
+            signInEmailEditText.requestFocus();
+            return;
+        }
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            signInEmailEditText.setError("Enter a valid email address");
+            signInEmailEditText.requestFocus();
+            return;
+        }
+
+        //checking the validity of the password
+        if (email.isEmpty()) {
+            signInPasswordEditText.setError("Enter a password");
+            signInPasswordEditText.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if (task.isSuccessful()) {
+                    finish();
+                    Intent in = new Intent(getApplicationContext(), StudentDashBoardActivity.class);
+                    in.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(in);
+                } else {
+
+                    Toast.makeText(getApplicationContext(), "Login Unsuccessful", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+
+    }
 }
