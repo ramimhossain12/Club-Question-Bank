0affc029624fb2186454c72667d5bd33428e48e3
ramimhossain12 <ramimhossain6242@gmail.com>
2021-01-21 22:06:09 +0600

    UI update 1

--- app/src/main/java/com/example/programmingquestionbank/MainActivity.java
+++ app/src/main/java/com/example/programmingquestionbank/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setTitle("Choose Activity");
+        //getSupportActionBar().setTitle("Choose Activity");
 
 
         //find button.......
